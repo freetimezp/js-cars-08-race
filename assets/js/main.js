26ff21c8614704d046aba435b767fe964fcf5bb5
freetimezp <kraftukrnet@ukr.net>
2024-07-10 22:44:34 +0300

    04. js-cars-08-race. Swiper slider.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,20 @@ const linkAction = () => {
 navLinks.forEach(link => link.addEventListener("click", linkAction));
 
 /*=============== SWIPER CAR ===============*/
+const swiperHome = new Swiper(".home__swiper", {
+    speed: 1200,
+    effect: 'fade',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: (index, className) => {
+            return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + '</span>';
+        }
+    }
+});
+
+
 
 
 /*=============== GSAP ANIMATION ===============*/
